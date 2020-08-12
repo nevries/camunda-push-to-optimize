491c2ca99475fe48df7cb167631778952817dc73
Ragnar Nevries <ragnar.nevries@camunda.com>
2020-08-12 04:53:27 -0600

    running demo application with simulator

---
+++ src/main/java/com/camunda/consulting/camunda_bpm_simulator_example/CamundaApplication.java
@@ -0,0 +1,33 @@
+package com.camunda.consulting.camunda_bpm_simulator_example;
+
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.joda.time.DateTime;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.camunda.consulting.simulator.PayloadGenerator;
+import com.camunda.consulting.simulator.SimulationExecutor;
+import com.camunda.consulting.simulator.SimulatorPlugin;
+
+@SpringBootApplication
+@EnableProcessApplication
+public class CamundaApplication {
+
+  public static void main(String... args) {
+    SpringApplication.run(CamundaApplication.class, args);
+  }
+
+  /**
+   * Makes the PayloadGenerator available in expressions and scripts.
+   */
+  @Bean
+  public PayloadGenerator generator() {
+    return new PayloadGenerator();
+  }
+
+  @Bean
+  public SimulatorPlugin simulatorPlugin() {
+    return new SimulatorPlugin();
+  }
+}
